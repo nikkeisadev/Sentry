2c1b08314a935731dbb075217e57a2f0dbc21249
nikkeisadev <notnikkecrd@gmail.com>
2023-11-12 14:03:34 +0100

    Start up animation finished! ✔

--- source/boot_screen.py
+++ source/boot_screen.py
@@ -1,31 +1,35 @@
-from PyQt5.QtCore import Qt 
-from PyQt5.QtWidgets import QLabel, QMainWindow, QApplication, QWidget, QVBoxLayout
-from PyQt5.QtGui import QPixmap
-import sys 
-  
-  
-class Window(QMainWindow): 
-    def __init__(self): 
-        super().__init__() 
-        self.setWindowFlag(Qt.FramelessWindowHint) 
-        
-        self.central_widget = QWidget()               
-        self.setCentralWidget(self.central_widget)    
-        lay = QVBoxLayout(self.central_widget)
-        
-        label = QLabel(self)
-        pixmap = QPixmap('images/startup_logo.png')
-        label.setPixmap(pixmap)
-        self.resize(pixmap.width(), pixmap.height())
-        
-        lay.addWidget(label)
-        self.show()
-  
-  
-# create pyqt5 app 
-App = QApplication(sys.argv) 
-  
-# create the instance of our Window 
-window = Window() 
-# start the app 
-sys.exit(App.exec()) 
+import os
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import Qt, QTimer
+
+scriptDir = os.path.dirname(os.path.realpath(__file__))
+gifFile = (scriptDir + os.path.sep + 'images/startup_logo.gif')
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.resize(384, 242)
+        self.MovieLabel = QtWidgets.QLabel(self)
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.MovieLabel.setGeometry(QtCore.QRect(0, 0, 384, 242))
+        self.movie = QMovie(gifFile)
+        self.MovieLabel.setMovie(self.movie)
+        self.movie.start()
+
+        # Start a QTimer with a delay of 3 seconds (3000 milliseconds)
+        self.timer = QTimer()
+        self.timer.setSingleShot(True)
+        self.timer.timeout.connect(self.closeApplication)
+        self.timer.start(4000)  # Delay of 3 seconds
+
+    def closeApplication(self):
+        # Perform any cleanup or additional actions before closing the application
+        # Here, we simply exit the application
+        QtWidgets.QApplication.quit()
+
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec_())
